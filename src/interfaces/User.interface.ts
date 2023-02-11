39dbd0a726f694354487e05c6168b4b22265337a
Filipe Lima <cimofelipe4@gmail.com>
2023-02-11 15:56:54 -0300

    feat: create user model

---
+++ src/interfaces/User.interface.ts
@@ -0,0 +1,7 @@
+export interface Iuser {
+  id?: number,
+  username: string,
+  vocation: string,
+  level: number,
+  password: string
+}
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,3 +1,20 @@
+import { ResultSetHeader } from 'mysql2';
+import connection from './connection';
+import { Iuser } from '../interfaces/User.interface';
+
 export default class User {
-  
+  static async create(people: Iuser): Promise<Iuser> {
+    const columns = Object.keys(people).join(', ');
+
+    const placeholders = Object.keys(people)
+      .map((_key) => '?')
+      .join(', ');
+
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(
+      `INSERT INTO Trybesmith.users (${columns}) VALUE (${placeholders})`,
+      [...Object.values(people)],
+    );
+
+    return { id: insertId, ...people };
+  }
 }
