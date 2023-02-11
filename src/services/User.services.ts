0e2b5b1fd7d97dda7e50e71c24d21af5fef6b726
Filipe Lima <cimofelipe4@gmail.com>
2023-02-11 16:02:57 -0300

    feat: created services user

--- src/services/User.services.ts
+++ src/services/User.services.ts
@@ -1,3 +1,10 @@
-export default class User {
-  
+import { Iuser } from '../interfaces/User.interface';
+
+import { userModel } from '../models';
+
+export default class UserServices {
+  static async create(user: Iuser): Promise<Iuser> {
+    const createdPeople = await userModel.create(user);
+    return createdPeople;
+  }
 }
